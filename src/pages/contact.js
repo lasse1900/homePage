e5f5f81be3d988d4c5ce8b917359287f21787c0a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-23 22:12:37 +0200

    some refactoring

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,8 +6,8 @@ const ContactPage = () => {
     <Layout>
       <h1>Contact Page</h1>
       <p>You can contact me through LinkenIn or Twitter:</p>
-      <p>Mail to me<a href="https://www.linkedin.com/in/laurikyttala/" target="_blank" rel="noopener noreferrer">@laurikyt</a> on LinkeIn</p>
-      <p>Want to tweet to me <a href="https://twitter.com/laurikyt" target="_blank" rel="noopener noreferrer">@laurikyt</a> on Twitter</p>
+      <p>Mail to me <a href="https://www.linkedin.com/in/laurikyttala/" target="_blank" rel="noopener noreferrer">@laurikyttala</a> on LinkeIn</p>
+      <p>Want to tweet <a href="https://twitter.com/laurikyt" target="_blank" rel="noopener noreferrer">@laurikyt</a> on Twitter</p>
     </Layout>
   )
 }
